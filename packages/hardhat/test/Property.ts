7d17a2a811367f22df0c79e7046ccc4e079b1b5c
Mohammad Amir <amir@connextar.com>
2024-12-09 22:20:35 +0000

    Started to add tests.

--- packages/hardhat/test/Property.ts
+++ packages/hardhat/test/Property.ts
@@ -1,8 +1,8 @@
-import { expect } from "chai";
-import { ZeroAddress } from "ethers";
-import { ethers, upgrades } from "hardhat";
-import { Property } from "@typechain-types";
-import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
+import {expect} from "chai";
+import {ZeroAddress} from "ethers";
+import {ethers, upgrades} from "hardhat";
+import {Property} from "@typechain-types";
+import {SignerWithAddress} from "@nomicfoundation/hardhat-ethers/signers";
 
 describe("Property", function () {
   let property: Property;
@@ -39,9 +39,22 @@ describe("Property", function () {
     });
 
     it("Should not allow non-minters to mint tokens", async function () {
-      await expect(property.connect(user).mint(user.address, 1, 100, "0x")).to.be.revertedWith(
-        "AccessControl: account " + user.address.toLowerCase() + " is missing role " + (await property.MINTER_ROLE()),
-      );
+      // try {
+      //   await property.connect(user).mint(user.address, 1, 100, "0x");
+      // } catch (error) {
+      //   // Gives error like: AccessControlUnauthorizedAccount("0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC", "0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6")
+      //   console.error("Mint error", error);
+      // }
+      // The test will fail here, but we'll see the error in the console
+      // expect(true).to.equal(false); // This line ensures the test fails, and we see the error
+
+      // await expect(property.connect(user).mint(user.address, 1, 100, "0x")).to.be.revertedWith(
+      //   "AccessControl: account " + user.address.toLowerCase() + " is missing role " + (await property.MINTER_ROLE()),
+      // );
+
+      await expect(property.connect(user).mint(user.address, 1, 100, "0x"))
+        .to.be.revertedWithCustomError(property, "AccessControlUnauthorizedAccount")
+        .withArgs(user.address, await property.MINTER_ROLE());
     });
 
     it("Should allow minter to batch mint tokens", async function () {
@@ -87,12 +100,25 @@ describe("Property", function () {
     });
 
     it("Should not allow non-URI_SETTER_ROLE to set URI", async function () {
-      await expect(property.connect(user).setURI("https://example.com/token/")).to.be.revertedWith(
-        "AccessControl: account " +
-          user.address.toLowerCase() +
-          " is missing role " +
-          (await property.URI_SETTER_ROLE()),
-      );
+      // try {
+      //   await property.connect(user).setURI("https://example.com/token/");
+      // } catch (error) {
+      //   // Gives error like: AccessControlUnauthorizedAccount("0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC", "0x7804d923f43a17d325d77e781528e0793b2edd9890ab45fc64efd7b4b427744c")
+      //   console.error("setURI error", error);
+      // }
+      // // The test will fail here, but we'll see the error in the console
+      // expect(true).to.equal(false); // This line ensures the test fails, and we see the error
+
+      // await expect(property.connect(user).setURI("https://example.com/token/")).to.be.revertedWith(
+      //   "AccessControl: account " +
+      //   user.address.toLowerCase() +
+      //     " is missing role " +
+      //   (await property.URI_SETTER_ROLE()),
+      // );
+
+      await expect(property.connect(user).setURI("https://example.com/token/"))
+        .to.be.revertedWithCustomError(property, "AccessControlUnauthorizedAccount")
+        .withArgs(user.address, await property.URI_SETTER_ROLE());
     });
   });
 
