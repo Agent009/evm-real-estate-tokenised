d3289aeea13497456e8fe0fb568548981fddcbb2
Mohammad Amir <amir@connextar.com>
2024-12-17 01:36:51 +0000

    Improvements to the deployment script.

--- packages/hardhat/deploy/00_deploy_all_contracts.ts
+++ packages/hardhat/deploy/00_deploy_all_contracts.ts
@@ -2,11 +2,11 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 
 /**
- * Deploys the "Property" contract using the deployer account
+ * Deploys all of the contracts using the deployer account
  *
  * @param hre HardhatRuntimeEnvironment object.
  */
-const deployPropertyRwa: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+const deployAllContracts: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   /*
     On localhost, the deployer account is the one that comes with Hardhat, which is already funded.
 
@@ -17,8 +17,8 @@ const deployPropertyRwa: DeployFunction = async function (hre: HardhatRuntimeEnv
     with a random private key in the .env file (then used on hardhat.config.ts)
     You can run the `yarn account` command to check your balance in every network.
   */
-
   const { deployer } = await hre.getNamedAccounts();
+  // const { deploy } = hre.deployments;
   const { deployProxy } = hre.upgrades;
 
   if (!deployer) {
@@ -30,6 +30,7 @@ const deployPropertyRwa: DeployFunction = async function (hre: HardhatRuntimeEnv
   // Property contract
   //
   const Property = await hre.ethers.getContractFactory("Property");
+  // Deploy the proxy contract
   const property = await deployProxy(Property, [deployer, deployer]);
   await property.waitForDeployment();
 
@@ -37,7 +38,7 @@ const deployPropertyRwa: DeployFunction = async function (hre: HardhatRuntimeEnv
   if (!propertyAddress) {
     throw new Error("Deployment failed for Property, no contract address found.");
   }
-  console.log("Property Address: ", propertyAddress);
+  // console.log("Property Address: ", propertyAddress);
 
   const deployedProperty = await hre.ethers.getContractAt("Property", propertyAddress);
   if (!deployedProperty) {
@@ -57,7 +58,7 @@ const deployPropertyRwa: DeployFunction = async function (hre: HardhatRuntimeEnv
   if (!propertyTokenAddress) {
     throw new Error("Deployment failed for PropertyToken, no contract address found.");
   }
-  console.log("Property Token Address: ", propertyTokenAddress);
+  // console.log("Property Token Address: ", propertyTokenAddress);
 
   const deployedPropertyToken = await hre.ethers.getContractAt("PropertyToken", propertyTokenAddress);
   if (!deployedPropertyToken) {
@@ -65,6 +66,26 @@ const deployPropertyRwa: DeployFunction = async function (hre: HardhatRuntimeEnv
   }
 
   console.log(`👋 PropertyToken contract deployed at ${propertyTokenAddress} by deployer ${deployer}`);
+
+  //
+  // Payment Token Mock contract
+  //
+  const PaymentTokenMock = await hre.ethers.getContractFactory("PaymentTokenMock");
+  const paymentToken = await PaymentTokenMock.deploy("MockToken", "MTK");
+  await paymentToken.waitForDeployment();
+
+  const paymentTokenAddress = await propertyToken.getAddress();
+  if (!paymentTokenAddress) {
+    throw new Error("Deployment failed for PropertyToken, no contract address found.");
+  }
+  // console.log("Payment Token Mock Address: ", paymentTokenAddress);
+
+  const deployedPaymentToken = await hre.ethers.getContractAt("PaymentTokenMock", paymentTokenAddress);
+  if (!deployedPaymentToken) {
+    throw new Error("No Contract deployed with name: PaymentTokenMock");
+  }
+
+  console.log(`👋 PaymentTokenMock contract deployed at ${paymentTokenAddress} by deployer ${deployer}`);
 
   //
   // Add Property contract
@@ -77,7 +98,7 @@ const deployPropertyRwa: DeployFunction = async function (hre: HardhatRuntimeEnv
   if (!addPropertyAddress) {
     throw new Error("Deployment failed for AddProperty, no contract address found.");
   }
-  console.log("Add Property Address: ", addPropertyAddress);
+  // console.log("Add Property Address: ", addPropertyAddress);
 
   const deployedAddProperty = await hre.ethers.getContractAt("AddProperty", addPropertyAddress);
   if (!deployedAddProperty) {
@@ -90,7 +111,12 @@ const deployPropertyRwa: DeployFunction = async function (hre: HardhatRuntimeEnv
   // Property Vault contract
   //
   const PropertyVault = await hre.ethers.getContractFactory("PropertyVault");
-  const propertyVault = await PropertyVault.deploy(addPropertyAddress, propertyAddress, propertyTokenAddress);
+  const propertyVault = await PropertyVault.deploy(
+    addPropertyAddress,
+    propertyAddress,
+    propertyTokenAddress,
+    paymentTokenAddress,
+  );
   await propertyVault.waitForDeployment();
 
   const propertyVaultAddress = await propertyVault.getAddress();
@@ -107,8 +133,8 @@ const deployPropertyRwa: DeployFunction = async function (hre: HardhatRuntimeEnv
   console.log(`👋 PropertyVault contract deployed at ${propertyVaultAddress} by deployer ${deployer}`);
 };
 
-export default deployPropertyRwa;
+export default deployAllContracts;
 
 // Tags are useful if you have multiple deploy files and only want to run one of them.
 // e.g. yarn deploy --tags PropertyRWA
-deployPropertyRwa.tags = ["PropertyRWA"];
+deployAllContracts.tags = ["PropertyRWA"];
